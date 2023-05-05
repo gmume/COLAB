7d7856accc23c38c02d601ca9b6c535f271dc7e3
Melanie Gmünder <melanie.gmuender@stud.hslu.ch>
2023-05-05 09:36:49 +0200

    Deletet _config.yml

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,31 +1,38 @@
 let rot = 0;
 let x = 0;
 let y = 0;
+let angle = 0;
+
+let countUpX = 0;
+let countUpY = 0;
+let myMatrix;
+
+// let sliderX;
+// let sliderY;
 
 function setup(){
   createCanvas(window.innerWidth, window.innerHeight);
 
   background(0);
   frameRate(10);
-  stroke(100, 100, 100);
-  strokeWeight(5);
+  stroke(100, 100, 100, 100);
+  strokeWeight(1);
   noFill();
+
+  // sliderX = createSlider(0.001, 1, 0.01, 0.01);
+  // sliderX.position(10, 10);
+  // sliderX.style('width', width / 10 + "px");
+
+  // sliderY = createSlider(0.001, 1, 0.01, 0.01);
+  // sliderY.position(10, 30);
+  // sliderY.style('width', width / 10 + "px");
 }
 
 function draw(){
-  resetMatrix();
-  applyMatrix(1, rot, -rot, 1, x, y)
-  bezier(850, 400, 100, 100, 900, 900, 350, 1200);
 
-  rot += map(noise(rot), 0, 1, 0, 0.1);
-  x += noise(x / 100);
-  y -= noise(y / 100);
-}
+  rotate(angle);
+  bezier(width / 2 - 40, height / 2 - 40, width / 2 + 80, height / 2 - 80, width / 2 + 200, height / 2 + 10, width / 2 + 40, height / 2 +40);
 
-function path() {
-  //create random path with x,y within canvas
-}
-
-function rotation() {
-  //create randomly turning rotation
+  angle += 0.001;
+  console.log("frameCount: "+frameCount);
 }
