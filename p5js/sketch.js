8740d74015290abc2abad9ad94efcdb6bea859aa
Melanie Gmünder <melanie.gmuender@stud.hslu.ch>
2023-05-08 16:07:59 +0200

    Update sketch.js

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,38 +1,77 @@
 let rot = 0;
-let x = 0;
-let y = 0;
 let angle = 0;
+let countSteps;
+let oldFrameCount;
+let directionFactorX;
+let directionFactorY;
+let shiftX;
+let shiftY;
+let lineStart;
+let controlPoint1;
+let lineMiddle;
+let controlPoint2;
+let lineEnd;
 
-let countUpX = 0;
-let countUpY = 0;
-let myMatrix;
-
-// let sliderX;
-// let sliderY;
-
-function setup(){
+function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-
-  background(0);
-  frameRate(10);
+  frameRate(20);
+  stroke("black");
+  strokeWeight(5);
+  angleMode(DEGREES);
+  noFill();
   stroke(100, 100, 100, 100);
   strokeWeight(1);
-  noFill();
 
-  // sliderX = createSlider(0.001, 1, 0.01, 0.01);
-  // sliderX.position(10, 10);
-  // sliderX.style('width', width / 10 + "px");
-
-  // sliderY = createSlider(0.001, 1, 0.01, 0.01);
-  // sliderY.position(10, 30);
-  // sliderY.style('width', width / 10 + "px");
+  countSteps = random(5, 100);
+  oldFrameCount = frameCount;
+  shiftX = 1;
+  shiftY = 1;
+  directionFactorX = 1;
+  directionFactorY = 1;
+  lineStart     = createVector(0, 0);
+  controlPoint1 = createVector(random(0, 250), random(0, 250));
+  lineMiddle    = createVector(random(0, 250), random(0, 250));
+  controlPoint2 = createVector(random(0, 250), random(0, 250));
+  lineEnd       = createVector(random(0, 250), random(0, 250));
 }
 
-function draw(){
+function draw() {
 
-  rotate(angle);
-  bezier(width / 2 - 40, height / 2 - 40, width / 2 + 80, height / 2 - 80, width / 2 + 200, height / 2 + 10, width / 2 + 40, height / 2 +40);
+  if(frameCount > oldFrameCount + countSteps) {
+    changeDirection();
+  }
 
-  angle += 0.001;
-  console.log("frameCount: "+frameCount);
+  translate(width / 2 + shiftX, height / 2 + shiftY);
+  rotate(angle % 360);
+  createLine();
+  shiftX += 1 * directionFactorX;
+  shiftY += 1 * directionFactorY;
+  angle += 0.2;
+}
+
+function changeDirection() {
+  countSteps = random(20, 100);
+  directionFactorX = random([-1, 1]);
+  directionFactorY = random([-1, 1]);
+  oldFrameCount = frameCount;
+}
+
+function createLine() {
+  beginShape();
+  //Line start point
+  vertex(lineStart.x, lineStart.y);
+
+  quadraticVertex(
+    //control point 1
+    controlPoint1.x, controlPoint1.y,
+    //anchor point middle
+    lineMiddle.x, lineMiddle.y
+  );
+  quadraticVertex(
+    //control point 2
+    controlPoint2.x, controlPoint2.y,
+    //anchor point end
+    lineEnd.x, lineEnd.y
+  );
+  endShape();
 }
