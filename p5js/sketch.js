3a6528197bcb0fe203d503c817f7b2b03df7ece3
Melanie Gmünder <melanie.gmuender@stud.hslu.ch>
2023-05-09 11:51:09 +0200

    Added box

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -12,7 +12,6 @@ let controlPoint1;
 let lineMiddle;
 let controlPoint2;
 let lineEnd;
-
 let sliderAngle;
 let sliderCountStepsMin;
 let sliderCountStepsMax;
@@ -29,21 +28,25 @@ function setup() {
   group.position(30, 30);  
   
   sliderAngle = createSlider(0.0, 0.5, 0.3, 0.1);
+  sliderAngle.style('width', '80px');
   sliderAngle.parent(group);
   labelSliderAngle = createSpan("angle&#8195");
   labelSliderAngle.parent(group);
   
   sliderCountStepsMin = createSlider(10, 200, 100, 10);
+  sliderCountStepsMin.style('width', '80px');
   sliderCountStepsMin.parent(group);
   labelSliderCountStepsMin = createSpan("min step&#8195");
   labelSliderCountStepsMin.parent(group);
 
   sliderCountStepsMax = createSlider(200, 500, 300, 10);
+  sliderCountStepsMax.style('width', '80px');
   sliderCountStepsMax.parent(group);
   labelSliderCountStepsMax = createSpan("max step&#8195");
   labelSliderCountStepsMax.parent(group);
 
   sliderSpeed = createSlider(10, 100, 30, 10);
+  sliderSpeed.style('width', '80px');
   sliderSpeed.parent(group);
   labelSliderSpeed = createSpan('speed');
   labelSliderSpeed.parent(group);
@@ -65,6 +68,8 @@ function setup() {
 }
 
 function draw() {
+  background(360);
+
   if (frameCount > oldFrameCount + countSteps) {
     changeDirection();
   }
@@ -75,15 +80,27 @@ function draw() {
   rotate(angle % 360);
   createLine();
 
-  if(shiftX + 1 * directionFactorX < width / 2 - sizeLine * 1.2 && shiftX + 1 * directionFactorX >  sizeLine * 2.2 - width / 2) {
+  //box
+  line(0, 0, sizeLine, 0);
+  line(sizeLine, 0, sizeLine, sizeLine);
+  line(sizeLine, sizeLine, 0, sizeLine);
+  line(0,sizeLine, 0, 0);
+
+  // if(shiftX + 1 * directionFactorX < width / 2 - sizeLine * 1.2 && shiftX + 1 * directionFactorX >  sizeLine * 2.2 - width / 2) {
+  if(shiftX + 1 * directionFactorX < width / 2 - sizeLine * 1.2) {
+    console.log("entred if x");
     shiftX += 1 * directionFactorX;
   } else {
+    console.log("entred else x");
     shiftX -= 1 * directionFactorX;
   }
   
-  if(shiftY + 1 * directionFactorY < height / 2 - sizeLine * 1.2 && shiftY + 1 * directionFactorY >   sizeLine * 2.2 - height / 2) {
+  // if(shiftY + 1 * directionFactorY < height / 2 - sizeLine * 1.2 && shiftY + 1 * directionFactorY >   sizeLine * 2.2 - height / 2) {
+  if(shiftY + 1 * directionFactorY < height / 2 - sizeLine * 1.2) {
+    console.log("entred if y");
     shiftY += 1 * directionFactorY;
   } else {
+    console.log("entred else y");
     shiftY -= 1 * directionFactorY;
   }
   
