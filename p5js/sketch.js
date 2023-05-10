cec2309a3c9a54e7f33699c43f5557ff93d28c99
Melanie Gmünder <melanie.gmuender@stud.hslu.ch>
2023-05-10 13:50:44 +0200

    Update sketch.js

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -2,50 +2,59 @@ let sizeLine;
 let countSteps;
 let oldFrameCount;
 
-let shiftX;
-let shiftY;
-let directionFactorX;
-let directionFactorY;
+let shiftX, shiftY;
+let directionFactorX, directionFactorY;
 
-let lineStart;
-let controlPoint1;
-let lineMiddle;
-let controlPoint2;
-let lineEnd;
+let lineStart, controlPoint1, lineMiddle, controlPoint2, lineEnd;
 
 let pivot;
-let minX;
-let maxX;
-let minY;
-let maxY;
-
-let outlineMiddleRight;
-let outlineMiddleLeft; 
-let outlineMiddleLower;
-let outlineMiddleUpper;
+let minX, maxX, minY, maxY;
+let outlineMiddleRight, outlineMiddleLeft, outlineMiddleLower, outlineMiddleUpper;
 
 function setup() {
   createCanvas(400, 500);
   noFill();
+  stroke(100, 60);
   strokeWeight(1);
   background(230);
   frameRate(500);
   
-  sizeLine = 150;
   countSteps = random(300, 500);
   oldFrameCount = frameCount;
 
+  setupCanvasAdjustment();
+  setupLine();
+  setupBoundaries();
+}
+
+function draw() {
+  adjustOutlines();
+  dontCrossOutlines();
+  translate(width / 2 + shiftX, height / 2 + shiftY);
+  createLine();
+  
+  if (frameCount > oldFrameCount + countSteps) {
+    changeDirection();
+  }
+}
+
+function setupCanvasAdjustment() {
   shiftX = 1;
   shiftY = 1;
   directionFactorX = 1;
   directionFactorY = 1;
+}
 
+function setupLine() {
+  sizeLine = 150;
   lineStart     = createVector(0, 0);
   controlPoint1 = createVector(random(0, sizeLine), random(0, sizeLine));
   lineMiddle    = createVector(random(0, sizeLine), random(0, sizeLine));
   controlPoint2 = createVector(random(0, sizeLine), random(0, sizeLine));
   lineEnd       = createVector(random(0, sizeLine), random(0, sizeLine));
+}
 
+function setupBoundaries() {
   pivot = createVector(0, 0);
   minX = (pivot.x - width  / 2) * 0.9;
   maxX = (pivot.x + width  / 2) * 0.9;
@@ -53,22 +62,14 @@ function setup() {
   maxY = (pivot.y + height / 2) * 0.9;
 }
 
-function draw() {
-
-  if (frameCount > oldFrameCount + countSteps) {
-    changeDirection();
-  }
-
-  frameRate(300);
-  stroke(100, 60);
-  translate(width / 2 + shiftX, height / 2 + shiftY);
-  createLine();
-  
+function adjustOutlines() {
   outlineMiddleRight = createVector(maxX - shiftX, minY - shiftY + height / 2 * 0.9);
   outlineMiddleLeft  = createVector(minX - shiftX, minY - shiftY + height / 2 * 0.9);
   outlineMiddleLower = createVector(minX - shiftX + width / 2 * 0.9, maxY - shiftY);
   outlineMiddleUpper = createVector(minX - shiftX + width / 2 * 0.9, minY - shiftY);
+}
 
+function dontCrossOutlines() {
   if(pivot.x < outlineMiddleRight.x - sizeLine && outlineMiddleLeft.x < pivot.x) {
     shiftX += 1 * directionFactorX;
   } else {
@@ -89,7 +90,6 @@ function changeDirection() {
   directionFactorX = map(noise(random(0, 10)), 0, 1, -1, 1);
   directionFactorY = map(noise(random(0, 10)), 0, 1, -1, 1);
   oldFrameCount = frameCount;
-
   background(360, 10);
 }
 
